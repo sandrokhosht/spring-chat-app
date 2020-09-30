23fa74bb269e477eaf208e57587bb2200228a4fb
Aleksandre <sandrokhosht@gmail.com>
2020-10-01 00:34:58 +0400

    Initial commit

---
+++ src/main/java/com/example/springchatapp/SpringChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatAppApplication.class, args);
+	}
+
+}
